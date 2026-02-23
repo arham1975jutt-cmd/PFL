96cbc8a75cfc338d213dcffc21186d5bd4210668
Muhammad Arham <arham1975jutt@gmail.com>
2026-02-23 13:21:32 +0500

    pf codes

---
+++ 1/p13.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){  
+cout<<"                                                             "<<endl;
+cout<<"        +--^----------- ------ ------- -------^-,                                                "<<endl;
+cout<<"         |  |||||||||  '-------'      |         o       "<<endl;
+cout<<"        '+----------------------------^--------|      "<<endl;
+cout<<"          '----------- -------- --------------'                                             "<<endl;
+cout<<"           / xxxxxx /                                     "<<endl;
+cout<<"          / xxxxxx /                                      "<<endl;
+cout<<"         / xxxxxx /                                      "<<endl;
+cout<<"        / xxxxxx /                                       "<<endl; 
+cout<<"       / xxxxxx /                                       "<<endl; 
+cout<<"      / xxxxxxx/                                        "<<endl;
+cout<<"     (_ _ _ _ _)                                                "<<endl;
+cout<<"     '---------'                                              "<<endl;
+cout<<"                                                         "<<endl;
+}  
---
+++ 1/p9.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){  
+cout<<"   --------------------------------------------------"<<endl;
+cout<<"                                                     "<<endl;
+cout<<"           O        ^_____^                 "<<endl;
+cout<<"               OE     (O O)\\___________          "<<endl;
+cout<<"                     (___)\\           )\\/\\       "<<endl;
+cout<<"                           ||--------w |          "<<endl;
+cout<<"                           ||         ||         "<<endl;
+cout<<"            "<<endl;
+
+}
